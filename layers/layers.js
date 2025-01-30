3c0e9f804c0395d1f82848fa1b6027cf9c100699
Moussa Bréhima Siby <moussabrehimasiby@gmail.com>
2025-01-30 20:40:13 +0000

    Add files via upload

--- layers/layers.js
+++ layers/layers.js
@@ -1,7 +1,18 @@
 var wms_layers = [];
 
 
-        var lyr_EsriSatellite_0 = new ol.layer.Tile({
+        var lyr_OpenStreetMap_0 = new ol.layer.Tile({
+            'title': 'OpenStreetMap',
+            'opacity': 1.000000,
+            
+            
+            source: new ol.source.XYZ({
+            attributions: ' ',
+                url: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png'
+            })
+        });
+
+        var lyr_EsriSatellite_1 = new ol.layer.Tile({
             'title': 'Esri Satellite',
             'opacity': 1.000000,
             
@@ -11,63 +22,45 @@ var wms_layers = [];
                 url: 'https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}'
             })
         });
-var format_COMMUNES_1 = new ol.format.GeoJSON();
-var features_COMMUNES_1 = format_COMMUNES_1.readFeatures(json_COMMUNES_1, 
+var format_NINACADKOLOKANI_2 = new ol.format.GeoJSON();
+var features_NINACADKOLOKANI_2 = format_NINACADKOLOKANI_2.readFeatures(json_NINACADKOLOKANI_2, 
             {dataProjection: 'EPSG:4326', featureProjection: 'EPSG:3857'});
-var jsonSource_COMMUNES_1 = new ol.source.Vector({
+var jsonSource_NINACADKOLOKANI_2 = new ol.source.Vector({
     attributions: ' ',
 });
-jsonSource_COMMUNES_1.addFeatures(features_COMMUNES_1);
-var lyr_COMMUNES_1 = new ol.layer.Vector({
+jsonSource_NINACADKOLOKANI_2.addFeatures(features_NINACADKOLOKANI_2);
+var lyr_NINACADKOLOKANI_2 = new ol.layer.Vector({
                 declutter: false,
-                source:jsonSource_COMMUNES_1, 
-                style: style_COMMUNES_1,
-                popuplayertitle: "COMMUNES",
+                source:jsonSource_NINACADKOLOKANI_2, 
+                style: style_NINACADKOLOKANI_2,
+                popuplayertitle: 'NINACAD KOLOKANI',
                 interactive: true,
-                title: '<img src="styles/legend/COMMUNES_1.png" /> COMMUNES'
+                title: '<img src="styles/legend/NINACADKOLOKANI_2.png" /> NINACAD KOLOKANI'
             });
-var format_Parcelles_NINACAD_By_COMMUNE_2 = new ol.format.GeoJSON();
-var features_Parcelles_NINACAD_By_COMMUNE_2 = format_Parcelles_NINACAD_By_COMMUNE_2.readFeatures(json_Parcelles_NINACAD_By_COMMUNE_2, 
+var format_CercledeKolokani_3 = new ol.format.GeoJSON();
+var features_CercledeKolokani_3 = format_CercledeKolokani_3.readFeatures(json_CercledeKolokani_3, 
             {dataProjection: 'EPSG:4326', featureProjection: 'EPSG:3857'});
-var jsonSource_Parcelles_NINACAD_By_COMMUNE_2 = new ol.source.Vector({
+var jsonSource_CercledeKolokani_3 = new ol.source.Vector({
     attributions: ' ',
 });
-jsonSource_Parcelles_NINACAD_By_COMMUNE_2.addFeatures(features_Parcelles_NINACAD_By_COMMUNE_2);
-var lyr_Parcelles_NINACAD_By_COMMUNE_2 = new ol.layer.Vector({
+jsonSource_CercledeKolokani_3.addFeatures(features_CercledeKolokani_3);
+var lyr_CercledeKolokani_3 = new ol.layer.Vector({
                 declutter: false,
-                source:jsonSource_Parcelles_NINACAD_By_COMMUNE_2, 
-                style: style_Parcelles_NINACAD_By_COMMUNE_2,
-                popuplayertitle: "Parcelles_NINACAD_By_COMMUNE",
+                source:jsonSource_CercledeKolokani_3, 
+                style: style_CercledeKolokani_3,
+                popuplayertitle: 'Cercle de Kolokani',
                 interactive: true,
-                title: '<img src="styles/legend/Parcelles_NINACAD_By_COMMUNE_2.png" /> Parcelles_NINACAD_By_COMMUNE'
-            });
-var format_Parcelles_NINACAD_ND_3 = new ol.format.GeoJSON();
-var features_Parcelles_NINACAD_ND_3 = format_Parcelles_NINACAD_ND_3.readFeatures(json_Parcelles_NINACAD_ND_3, 
-            {dataProjection: 'EPSG:4326', featureProjection: 'EPSG:3857'});
-var jsonSource_Parcelles_NINACAD_ND_3 = new ol.source.Vector({
-    attributions: ' ',
-});
-jsonSource_Parcelles_NINACAD_ND_3.addFeatures(features_Parcelles_NINACAD_ND_3);
-var lyr_Parcelles_NINACAD_ND_3 = new ol.layer.Vector({
-                declutter: false,
-                source:jsonSource_Parcelles_NINACAD_ND_3, 
-                style: style_Parcelles_NINACAD_ND_3,
-                popuplayertitle: "Parcelles_NINACAD_ND",
-                interactive: true,
-                title: '<img src="styles/legend/Parcelles_NINACAD_ND_3.png" /> Parcelles_NINACAD_ND'
+                title: '<img src="styles/legend/CercledeKolokani_3.png" /> Cercle de Kolokani'
             });
 
-lyr_EsriSatellite_0.setVisible(true);lyr_COMMUNES_1.setVisible(true);lyr_Parcelles_NINACAD_By_COMMUNE_2.setVisible(true);lyr_Parcelles_NINACAD_ND_3.setVisible(true);
-var layersList = [lyr_EsriSatellite_0,lyr_COMMUNES_1,lyr_Parcelles_NINACAD_By_COMMUNE_2,lyr_Parcelles_NINACAD_ND_3];
-lyr_COMMUNES_1.set('fieldAliases', {'GID_4': 'GID_4', 'GID_0': 'GID_0', 'COUNTRY': 'COUNTRY', 'GID_1': 'GID_1', 'REGION': 'REGION', 'GID_2': 'GID_2', 'CERCLE': 'CERCLE', 'GID_3': 'GID_3', 'ARRONDISSE': 'ARRONDISSE', 'COMMUNE': 'COMMUNE', 'VARNAME_4': 'VARNAME_4', 'TYPE_4': 'TYPE_4', 'ENGTYPE_4': 'ENGTYPE_4', 'CC_4': 'CC_4', });
-lyr_Parcelles_NINACAD_By_COMMUNE_2.set('fieldAliases', {'NAME_1': 'NAME_1', 'NAME_2': 'NAME_2', 'NAME_3': 'NAME_3', 'COMMUNES': 'COMMUNES', 'TYPE_4': 'TYPE_4', });
-lyr_Parcelles_NINACAD_ND_3.set('fieldAliases', {'surface': 'surface', 'perimetre': 'perimetre', 'numero_dep': 'numero_dep', 'code_rga': 'code_rga', 'localite': 'localite', 'nom': 'nom', 'prenom': 'prenom', });
-lyr_COMMUNES_1.set('fieldImages', {'GID_4': 'TextEdit', 'GID_0': 'TextEdit', 'COUNTRY': 'TextEdit', 'GID_1': 'TextEdit', 'REGION': 'TextEdit', 'GID_2': 'TextEdit', 'CERCLE': 'TextEdit', 'GID_3': 'TextEdit', 'ARRONDISSE': 'TextEdit', 'COMMUNE': 'TextEdit', 'VARNAME_4': 'TextEdit', 'TYPE_4': 'TextEdit', 'ENGTYPE_4': 'TextEdit', 'CC_4': 'TextEdit', });
-lyr_Parcelles_NINACAD_By_COMMUNE_2.set('fieldImages', {'NAME_1': 'TextEdit', 'NAME_2': 'TextEdit', 'NAME_3': 'TextEdit', 'COMMUNES': 'TextEdit', 'TYPE_4': 'TextEdit', });
-lyr_Parcelles_NINACAD_ND_3.set('fieldImages', {'surface': 'TextEdit', 'perimetre': 'TextEdit', 'numero_dep': 'TextEdit', 'code_rga': 'TextEdit', 'localite': 'TextEdit', 'nom': 'TextEdit', 'prenom': 'TextEdit', });
-lyr_COMMUNES_1.set('fieldLabels', {'GID_4': 'hidden field', 'GID_0': 'hidden field', 'COUNTRY': 'hidden field', 'GID_1': 'hidden field', 'REGION': 'inline label - always visible', 'GID_2': 'hidden field', 'CERCLE': 'inline label - always visible', 'GID_3': 'hidden field', 'ARRONDISSE': 'inline label - always visible', 'COMMUNE': 'inline label - always visible', 'VARNAME_4': 'hidden field', 'TYPE_4': 'hidden field', 'ENGTYPE_4': 'hidden field', 'CC_4': 'hidden field', });
-lyr_Parcelles_NINACAD_By_COMMUNE_2.set('fieldLabels', {'NAME_1': 'hidden field', 'NAME_2': 'hidden field', 'NAME_3': 'hidden field', 'COMMUNES': 'inline label - always visible', 'TYPE_4': 'hidden field', });
-lyr_Parcelles_NINACAD_ND_3.set('fieldLabels', {'surface': 'inline label - always visible', 'perimetre': 'inline label - always visible', 'numero_dep': 'inline label - always visible', 'code_rga': 'inline label - always visible', 'localite': 'inline label - always visible', 'nom': 'inline label - always visible', 'prenom': 'inline label - always visible', });
-lyr_Parcelles_NINACAD_ND_3.on('precompose', function(evt) {
+lyr_OpenStreetMap_0.setVisible(true);lyr_EsriSatellite_1.setVisible(true);lyr_NINACADKOLOKANI_2.setVisible(true);lyr_CercledeKolokani_3.setVisible(true);
+var layersList = [lyr_OpenStreetMap_0,lyr_EsriSatellite_1,lyr_NINACADKOLOKANI_2,lyr_CercledeKolokani_3];
+lyr_NINACADKOLOKANI_2.set('fieldAliases', {'id': 'id', 'ninacad': 'ninacad', 'section': 'section', 'surface': 'surface', 'perimetre': 'perimetre', 'numeroparc': 'numeroparc', 'numero_dep': 'numero_dep', 'code_rga': 'code_rga', 'localite': 'localite', 'nom': 'nom', 'prenom': 'prenom', 'layer': 'layer', 'path': 'path', });
+lyr_CercledeKolokani_3.set('fieldAliases', {'NAME_1': 'NAME_1', 'NL_NAME_1': 'NL_NAME_1', 'NAME_2': 'NAME_2', 'VARNAME_2': 'VARNAME_2', 'NL_NAME_2': 'NL_NAME_2', 'TYPE_2': 'TYPE_2', 'ENGTYPE_2': 'ENGTYPE_2', 'CC_2': 'CC_2', 'HASC_2': 'HASC_2', });
+lyr_NINACADKOLOKANI_2.set('fieldImages', {'id': 'TextEdit', 'ninacad': 'TextEdit', 'section': 'TextEdit', 'surface': 'TextEdit', 'perimetre': 'TextEdit', 'numeroparc': 'TextEdit', 'numero_dep': 'TextEdit', 'code_rga': 'TextEdit', 'localite': 'TextEdit', 'nom': 'TextEdit', 'prenom': 'TextEdit', 'layer': 'TextEdit', 'path': 'TextEdit', });
+lyr_CercledeKolokani_3.set('fieldImages', {'NAME_1': 'TextEdit', 'NL_NAME_1': 'TextEdit', 'NAME_2': 'TextEdit', 'VARNAME_2': 'TextEdit', 'NL_NAME_2': 'TextEdit', 'TYPE_2': 'TextEdit', 'ENGTYPE_2': 'TextEdit', 'CC_2': 'TextEdit', 'HASC_2': 'TextEdit', });
+lyr_NINACADKOLOKANI_2.set('fieldLabels', {'id': 'hidden field', 'ninacad': 'inline label - always visible', 'section': 'hidden field', 'surface': 'inline label - always visible', 'perimetre': 'inline label - always visible', 'numeroparc': 'hidden field', 'numero_dep': 'inline label - always visible', 'code_rga': 'inline label - always visible', 'localite': 'inline label - always visible', 'nom': 'inline label - always visible', 'prenom': 'inline label - always visible', 'layer': 'hidden field', 'path': 'hidden field', });
+lyr_CercledeKolokani_3.set('fieldLabels', {'NAME_1': 'inline label - always visible', 'NL_NAME_1': 'inline label - always visible', 'NAME_2': 'inline label - always visible', 'VARNAME_2': 'hidden field', 'NL_NAME_2': 'hidden field', 'TYPE_2': 'hidden field', 'ENGTYPE_2': 'hidden field', 'CC_2': 'hidden field', 'HASC_2': 'hidden field', });
+lyr_CercledeKolokani_3.on('precompose', function(evt) {
     evt.context.globalCompositeOperation = 'normal';
 });
